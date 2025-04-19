586f7656aef932569b36fade212f37d10c5615ff
adityadubey-binaried <aditya.dubey@binaried.com>
2025-04-20 01:17:14 +0530

    updated import

--- server/utils_python/audio_utils.py
+++ server/utils_python/audio_utils.py
@@ -1,6 +1,6 @@
 # utils/audio_utils.py
-from moviepy.editor import VideoFileClip
-
+# from moviepy.editor import VideoFileClip
+from moviepy.video.io.VideoFileClip import VideoFileClip
 class AudioUtils:
     @staticmethod
     def convert_video_to_audio(video_file, audio_file):
